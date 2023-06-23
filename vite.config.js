7858ce0ff71bd21fc62245f3b41281f06987bee0
QMHLO <htetlinoo@qualymm.com>
2023-06-23 16:28:24 +0630

    backedd

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ import requireSupport from "vite-plugin-require-support";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    requireSupport({
-      filters: /.ts$|.js$|.tsx$|.vue$/,
-    }),
+    react(
+      requireSupport({
+        filters: /.ts$|.js$|.tsx$|.vue$/,
+      })
+    ),
   ],
 });
