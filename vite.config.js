59e1295ab90af9ee295433bc148aa5a9b4385776
QMHLO <htetlinoo@qualymm.com>
2023-06-23 16:23:01 +0630

    added

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import requireTransform from "vite-plugin-require-transform";
-
+import requireSupport from "vite-plugin-require-support";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), requireTransform({})],
+  plugins: [
+    react(),
+    requireSupport({
+      filters: /.ts$|.js$|.tsx$|.vue$/,
+    }),
+  ],
 });
