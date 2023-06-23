aa8d5b2eae7911c2e67d3fd1f91af7b0763542c6
QMHLO <htetlinoo@qualymm.com>
2023-06-23 16:27:32 +0630

    backed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import requireSupport from "vite-plugin-require-support";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    requireSupport({
+      filters: /.ts$|.js$|.tsx$|.vue$/,
+    }),
+  ],
 });
