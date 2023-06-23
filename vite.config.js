db2bd123bff27f459b411fdaf294cc57325c8301
QMHLO <htetlinoo@qualymm.com>
2023-06-23 16:29:06 +0630

    back-ed

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import requireSupport from "vite-plugin-require-support";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(
-      requireSupport({
-        filters: /.ts$|.js$|.tsx$|.vue$/,
-      })
-    ),
-  ],
+  plugins: [react()],
 });
