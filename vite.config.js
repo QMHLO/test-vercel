6b32dc6a76f5a493e66d126ffb0e44d0bbd4133f
QMHLO <htetlinoo@qualymm.com>
2023-06-23 16:24:26 +0630

    added test

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import requireSupport from "vite-plugin-require-support";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
